129600022a4673516835d863fbeb69932fc39894
김태현 <taehyk528@gmail.com>
2021-09-28 18:15:13 +0900

    ✨[FEAT] : 탭바 컨트롤러 시작점 지정

--- GitHubAPI-Sample/GitHubAPI-Sample/App/SceneDelegate.swift
+++ GitHubAPI-Sample/GitHubAPI-Sample/App/SceneDelegate.swift
@@ -7,46 +7,47 @@
 
 import UIKit
 
+import Then
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    // MARK: - Properties
+    
     var window: UIWindow?
-
+    
+    lazy var repoNavController = UINavigationController(rootViewController: RepoViewController()).then {
+        $0.view.backgroundColor = .systemBackground
+    }
+    
+    lazy var userNavController = UINavigationController(rootViewController: UserViewController()).then {
+        $0.view.backgroundColor = .systemBackground
+    }
+    
+    lazy var tabBarController: UITabBarController = {
+        let tabBarController = UITabBarController()
+        tabBarController.delegate = tabBarController
+        tabBarController.view.backgroundColor = .systemBackground
+        return tabBarController
+    }()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        configureControllers()
+
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    // MARK: - Private Helpers
+    
+    private func configureControllers() {
+        repoNavController.configureTabBar(
+          title: "Repositories",
+          systemImageName: "folder.fill.badge.plus"
+        )
+        userNavController.configureTabBar(title: "Current User", systemImageName: "person.fill")
+        tabBarController.viewControllers = [repoNavController, userNavController]
     }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
-
