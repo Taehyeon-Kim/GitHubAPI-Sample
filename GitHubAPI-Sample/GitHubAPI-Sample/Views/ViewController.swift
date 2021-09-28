35d33617a083c3a46ff60e11e76507be663e8607
김태현 <taehyk528@gmail.com>
2021-09-28 10:43:17 +0900

    ✨[FEAT] : GitHub Login Flow - Get Access Token

--- GitHubAPI-Sample/GitHubAPI-Sample/Views/ViewController.swift
+++ GitHubAPI-Sample/GitHubAPI-Sample/Views/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
+    
+    private var provider = OAuthProvider(providerID: "github.com")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
+    @IBAction func tapGitHubLoginButton() {
+        performOAuthLoginFlow(for: provider)
+    }
+    
+    private func performOAuthLoginFlow(for provider: OAuthProvider) {
+        provider.getCredentialWith(nil) { credential, error in
+          guard error == nil else { return }
+          guard let credential = credential else { return }
+          self.signIn(with: credential)
+        }
+    }
+    
+    private func signIn(with credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { result, error in
+            guard error == nil else { return }
+            guard let oauthCredential = result?.credential as? OAuthCredential else { return }
+            guard let accessToken = oauthCredential.accessToken else { return }
+            print(accessToken)
+        }
+    }
 }
