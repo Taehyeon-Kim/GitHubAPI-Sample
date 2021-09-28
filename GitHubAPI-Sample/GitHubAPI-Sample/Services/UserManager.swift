179daf8728bf2f5eb65a04a395e2dc1a73f9aa87
김태현 <taehyk528@gmail.com>
2021-09-28 21:30:38 +0900

    ♻️[[REFACTOR] : UserManager 변경 - completion 추가 - responseDecodable로 변경

--- GitHubAPI-Sample/GitHubAPI-Sample/Services/UserManager.swift
+++ GitHubAPI-Sample/GitHubAPI-Sample/Services/UserManager.swift
@@ -18,19 +18,30 @@ class UserManager {
     
     private init() {}
     
-    public func getUser() {
+    public func getUser(completion: @escaping ((UserModel) -> Void)) {
         let url = "https://api.github.com/user"
         let accessToken = KeychainSwift().get("accessToken") ?? ""
         let headers: HTTPHeaders = ["Accept": "application/vnd.github.v3+json",
                                     "Authorization": "token \(accessToken)"]
-
-        AF.request(url, method: .get, parameters: [:], headers: headers).responseJSON(completionHandler: { (response) in
-            switch response.result {
-            case .success(let json):
-                print(json as! [String: Any])
-            case .failure:
-                print("")
-            }
-        })
+        
+        AF.request(url, method: .get, parameters: [:], headers: headers)
+            .responseDecodable(of: UserModel.self, completionHandler: { response in
+                switch response.result {
+                case .success(let data):
+                    completion(data)
+                case .failure:
+                    print("fail")
+                }
+            })
+                    
+//        AF.request(url, method: .get, parameters: [:], headers: headers)
+//            .responseJSON(completionHandler: { (response) in
+//            switch response.result {
+//            case .success(let json):
+//                print(json as! [String: Any])
+//            case .failure:
+//                print("")
+//            }
+//        })
     }
 }
