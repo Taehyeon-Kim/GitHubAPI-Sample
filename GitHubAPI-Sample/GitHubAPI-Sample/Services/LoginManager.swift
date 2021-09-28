65b4d43714cd2613ea40fdde04a52c5b49183bb8
김태현 <taehyk528@gmail.com>
2021-09-28 12:49:36 +0900

    ✨[FEAT] : LoginManager

--- GitHubAPI-Sample/GitHubAPI-Sample/Services/LoginManager.swift
+++ GitHubAPI-Sample/GitHubAPI-Sample/Services/LoginManager.swift
@@ -5,4 +5,37 @@
 //  Created by taehy.k on 2021/09/28.
 //
 
-import Foundation
+import UIKit
+
+import Alamofire
+import Firebase
+import KeychainSwift
+
+class LoginManager {
+    
+    /// singleton
+    static let shared = LoginManager()
+    
+    private init() {}
+    
+    public func performOAuthLoginFlow(for provider: OAuthProvider) {
+        provider.getCredentialWith(nil) { credential, error in
+          guard error == nil else { return }
+          guard let credential = credential else { return }
+          self.signIn(with: credential)
+        }
+    }
+    
+    public func signIn(with credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { result, error in
+            guard error == nil else { return }
+            guard let oauthCredential = result?.credential as? OAuthCredential else { return }
+            guard let accessToken = oauthCredential.accessToken else { return }
+            KeychainSwift().set(accessToken, forKey: "accessToken")
+        }
+    }
+    
+    public func logout() {
+        KeychainSwift().clear()
+    }
+}
